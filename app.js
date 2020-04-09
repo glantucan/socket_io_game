4dfb34de3bf13c26dec0b476531933299316b3ce
glantucan <glantucan@gmail.com>
2020-04-09 13:01:56 +0100

    Cnaged port env config to heroku's

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ app.get('/', function (req, res) {
 // Serve any requested files from the client folder (for example the images used on index.html)
 app.use('/client', express.static(__dirname + '/client'));
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 2000
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || 'localhost'
-console.log(server_port, server_ip_address);
-http.listen(server_port, server_ip_address, function() {
+var server_port = process.env.PORT || 2000
+//var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || 'localhost'
+console.log(server_port);
+http.listen(server_port, function() {
     console.log('Listening on port 2000...');
 });
 
