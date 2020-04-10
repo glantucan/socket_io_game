a52cc2c06a997bc3288688d0031bfc359ab8c894
glantucan <glantucan@gmail.com>
2020-04-10 18:13:25 +0100

    Players can move. Added player list and Logger

--- app.js
+++ app.js
@@ -19,7 +19,68 @@ http.listen(server_port, function() {
 
 
 var playerList = [];
+var socketList = [];
 var lastId = 0;
+
+const Player = function (name, socket) {
+    var x = 250;
+    var y = 250;
+    var accel = 3;
+    var friction = 1.2;
+    var maxSpeed = 10;
+    var vx = 0;
+    var vy = 0;
+    var pressingRight = false;
+    var pressingLeft =  false;
+    var pressingUp = false;
+    var pressingDown = false;
+
+    function updatePosition () {
+        if ( pressingRight ) {
+            vx += vx <= maxSpeed ? accel : 0;
+        } 
+        else if ( pressingLeft ) {
+            vx -= vx >= -maxSpeed ? accel : 0;
+        } 
+        else if ( vx !== 0 ) {
+            vx = vx / friction;
+            if ( vx > -0.2 && vx < 0.2) {
+                vx = 0;
+            }
+        }
+
+        if ( pressingUp ) {
+            vy -= vy >= -maxSpeed ? accel : 0;
+        } 
+        else if ( pressingDown ) {
+            vy += vy <= maxSpeed ? accel : 0;
+        } 
+        else if ( vy !== 0) {
+            vy = vy / friction;
+            if ( vy > -0.1 && vy < 0.1) {
+                vy = 0;
+            }
+        }
+        
+        x += vx;
+        y += vy;
+        //console.log(me.name, vx, vy, x, y)
+
+    }
+
+    return {
+        id: lastId++, 
+        socket,
+        name,
+        getX() { return x; },
+        getY() { return y; },
+        set pressingRight(val) { pressingRight = val; },
+        set pressingLeft(val) { pressingLeft = val; },
+        set pressingUp(val) { pressingUp = val; },
+        set pressingDown(val) { pressingDown = val; },
+        updatePosition
+    };
+}
 
 const io = require('socket.io')(http, {});
 io.on('connection', function(socket) {
@@ -29,30 +90,90 @@ io.on('connection', function(socket) {
         x: 0,
         y: 0,
     }
-    playerList.push(currentPlayer);
 
-    console.log('socket connection registered for player', lastId++);
-    socket.on('disconnect', function(){
-        playerList = playerList.filter( (player) => player !== currentPlayer );
-    });
+    socket.on( 'join-game', 
+        function (data) {
+            var connectedPlayer = Player(data.name, socket);
+            sendMessage(data.name + ' joined the game');
+            
+            socket.emit('joined', connectedPlayer.id);
+
+            playerList.push(connectedPlayer);
+
+            sendData('players-list-update', playerList.map(
+                player => ({
+                    name: player.name,
+                    id: player.id,
+                })
+            ));
+            
+            socket.on('disconnect', disconnect);
+            socket.on('leave-game', leaveGame);
+            socket.on('key-press', onKeyPress);
+
+            function onKeyPress(data) {
+                if (connectedPlayer.id === data.playerId) {   
+                    if (      data.inputId === 'left') {
+                        connectedPlayer.pressingLeft = data.state;
+                    }
+                    else if ( data.inputId === 'right') {
+                        connectedPlayer.pressingRight = data.state;
+                    }
+                    else if ( data.inputId === 'down') {
+                        connectedPlayer.pressingDown = data.state;
+                    }
+                    else if ( data.inputId === 'up') {
+                        connectedPlayer.pressingUp = data.state;
+                    } 
+                }
+            }
+            function disconnect() {
+                leaveGame();
+                socketList = socketList.filter( (s) => s !== socket );
+            }
+            function leaveGame() {
+                playerList = playerList.filter( (p) => p !== connectedPlayer );
+                sendData('players-list-update', playerList.map(
+                    player => ({
+                        name: player.name,
+                        id: player.id,
+                    })
+                ));
+                sendMessage(connectedPlayer.name + ' is leaving' )
+            }
+        }
+    ) 
+    socketList.push(socket);
+    sendMessage('socket connection registered for anonymous watcher');
+    
 });
 
+function sendMessage(message) {
+    socketList.forEach((s) => {
+        s.emit('server-message', message)
+    });
+}
 
+function sendData(packetType, data) {
+    socketList.forEach((s) => {
+        s.emit(packetType, data);
+    });
+}
 
 setInterval(function() {
     var playersUpdate = [];
-    playersUpdate = playerList.map( function(player) {
-        player.x++;
-        player.y++;
+    playersUpdate = playerList.map( function(currentPlayer) {
+        currentPlayer.updatePosition();
+        
         return {
-            id: player.id,
-            x: player.x,
-            y: player.y
+            id: currentPlayer.id,
+            x: currentPlayer.getX(),
+            y: currentPlayer.getY()
         };
     });
 
-    playerList.forEach( function(player) {
-        player.socket.emit('new position', playersUpdate);
+    socketList.forEach( function(socket) {
+        socket.emit('new position', playersUpdate);
         //console.log("Emiting update for player", player.id);
     });
 }, 1000/25); //25 frames per second
