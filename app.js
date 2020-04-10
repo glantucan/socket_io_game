601d046e14be3dda69952f619eea5ea7c6ff8866
glantucan <glantucan@gmail.com>
2020-04-10 10:20:15 +0100

    Players can connect and are updated

--- app.js
+++ app.js
@@ -18,7 +18,41 @@ http.listen(server_port, function() {
 
 
 
+var playerList = [];
+var lastId = 0;
+
 const io = require('socket.io')(http, {});
 io.on('connection', function(socket) {
-    console.log('socket connection');
+    var currentPlayer = {
+        socket,
+        id: lastId,
+        x: 0,
+        y: 0,
+    }
+    playerList.push(currentPlayer);
+
+    console.log('socket connection registered for player', lastId++);
+    socket.on('disconnect', function(){
+        playerList = playerList.filter( (player) => player !== currentPlayer );
+    });
 });
+
+
+
+setInterval(function() {
+    var playersUpdate = [];
+    playersUpdate = playerList.map( function(player) {
+        player.x++;
+        player.y++;
+        return {
+            id: player.id,
+            x: player.x,
+            y: player.y
+        };
+    });
+
+    playerList.forEach( function(player) {
+        player.socket.emit('new position', playersUpdate);
+        //console.log("Emiting update for player", player.id);
+    });
+}, 1000/25); //25 frames per second
